f1b5d287c51ed67c8f8d14bb39e91c59e3ca937d
Cashby <106636487+BonelessSugar@users.noreply.github.com>
2023-04-10 11:42:43 -0400

    Update main.c

--- Lab9 Reading ADC Inputs Through Polling/main.c
+++ Lab9 Reading ADC Inputs Through Polling/main.c
@@ -1,1 +1,100 @@
-I'm just working here.
+#include <stm32l476xx.h>
+
+// Reads the configured ADC channel and returns its value.
+unsigned int adc_read(void);
+
+// Delays for approximately the given amount of microseconds.
+void delay_us(volatile int);
+int values[1008] = {0};
+
+
+int main(void)
+{
+	
+    // Use the CCIPR register to select the system clock as a source for the
+    // ADC clock, then enable the ADC clock through the AHB.
+    RCC->CCIPR &= ~(RCC_CCIPR_ADCSEL_Msk);
+    RCC->CCIPR |= 3 << RCC_CCIPR_ADCSEL_Pos;
+    RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;
+
+    // Enable the ADC's internal voltage regulator, with a delay for its
+    // startup time.
+    // ADVREGEN requires some bits in the CR register to be zero before being
+    // set, so we clear CR to zero first.
+    ADC1->CR = 0;
+    ADC1->CR = ADC_CR_ADVREGEN;
+    delay_us(20);
+
+    // Clear DIFSEL to do single-ended conversions.
+    ADC1->DIFSEL = 0;
+
+    // Begin a single-ended calibration and wait for it to complete.
+    // If the clock source for the ADC is not correctly configured, the
+    // processor may get stuck in the while loop.
+    ADC1->CR &= ~(ADC_CR_ADCALDIF); // Select single-ended calibration
+    ADC1->CR |= ADC_CR_ADCAL;
+    while ((ADC1->CR & ADC_CR_ADCAL) != 0);
+
+    // Enable the ADC and wait for it to be ready.
+    ADC1->CR |= ADC_CR_ADEN;
+    while ((ADC1->ISR & ADC_ISR_ADRDY) == 0);
+
+    // GPIO pin PC3 will be used as our ADC input.
+    // In order: enable the GPIOC clock, set PC3 to analog input mode,
+    // connect ADC channel one to its GPIO pin (PC0).
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
+    GPIOC->MODER |= 3 << GPIO_MODER_MODE0_Pos;
+    GPIOC->ASCR |= 1 << 0;
+
+    // Make channel one the first in the ADC's read sequence.
+    // Using '=' here also clears the L field of the register, giving the
+    // sequence a length of one.
+    ADC1->SQR1 = 1 << ADC_SQR1_SQ1_Pos;
+
+    // Configure the Nucleo's LED (pin PA5) for output.
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+    GPIOA->MODER &= ~(GPIO_MODER_MODE5_Msk);
+    GPIOA->MODER |= 1 << GPIO_MODER_MODE5_Pos;
+
+    // In a loop, read from the ADC channel and set the LED according to
+    // the result.
+
+		int sample = 0; 
+		int place = 0;
+		unsigned int reading;
+		while (place <= 997){
+			reading = adc_read();
+			if (sample >= 200){
+				values[place] = reading;
+				place += 1;
+				sample = 0;}
+			else {sample ++;}
+		}
+		
+    /*while (1) {
+        unsigned int reading = adc_read();
+
+        if (reading > 2048)
+            GPIOA->BSRR |= 1 << 5;
+        else
+            GPIOA->BRR |= 1 << 5;
+    }*/
+}
+
+unsigned int adc_read(void)
+{
+    ADC1->CFGR &= ~(ADC_CFGR_CONT);         // Single conversion
+    ADC1->CR |= ADC_CR_ADSTART;             // Start converting
+    while ((ADC1->ISR & ADC_ISR_EOC) == 0); // Wait for end-of-conversion
+    ADC1->ISR &= ~(ADC_ISR_EOC);            // Clear the status flag
+    return ADC1->DR;                        // Read the result
+}
+
+void delay_us(volatile int amount)
+{
+    // This loop should take around a microsecond to execute per iteration
+    // when running at the default speed of 4MHz.
+    while (amount > 0) {
+        --amount;
+    }
+}
